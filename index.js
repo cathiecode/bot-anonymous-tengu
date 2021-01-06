617c2a54ee851cd96b99ce6abbbb598f98e29169
cathiecode <cathiecode@gmail.com>
2021-01-07 01:49:27 +0900

    awaitを修正

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ client.on('message', async (msg) => {
   console.log("メッセージを受信")
   const pure_content = msg.content.replace(/<.+?>\s?|^\s|\s$/gm, "")
   const converted_content = pure_content.replace(/^/gm, ":japanese_goblin: ")
-  msg.channel.send(converted_content)
-  msg.delete();
+  await msg.channel.send(converted_content)
 });
 
 client.login(TOKEN);
